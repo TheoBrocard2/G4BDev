09640875255dc1313988a929a20b7577ac5839a8
MatteoDAC <145110524+MatteoDAC@users.noreply.github.com>
2023-11-18 10:42:11 +0100

    - Application du diagramme de classe
    Création de toutes les classes et méthodes

---
+++ G4bDev_Hôtellerie/G4bDev_Hôtellerie/Client.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4bDev_Hôtellerie
+{
+    public class Client
+    {
+        private string nom;
+        private string prenom;
+        private string nbTel;
+        private string adresse;
+
+        public Client(string nom, string prenom, string nbTel, string adresse)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.adresse = adresse;
+            this.nbTel = nbTel;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public string NbTel
+        {
+            get
+            {
+                return nbTel;
+            }
+            set
+            {
+                nbTel = value;
+            }
+        }
+
+        public string Adresse
+        {
+            get
+            {
+                return adresse;
+            }
+            set
+            {
+                adresse = value;
+            }
+        }
+
+
+        public void Enregistrer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifierInformations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AnnulerReservation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
---
+++ G4bDev_Hôtellerie/G4bDev_Hôtellerie/Form1.cs
@@ -0,0 +1,10 @@
+namespace G4bDev_Hôtellerie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
